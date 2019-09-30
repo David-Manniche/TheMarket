7d2bac64008358372094e6dfcb3458f1fa673dd6
Tanima Bansal <tanima.bansal@maxixx.com>
2019-09-30 19:24:13 +0530

    task-66702-working on vendor subscription unit test cases

--- tests/testcases/UserPrivilegeTest.php
+++ tests/testcases/UserPrivilegeTest.php
@@ -18,10 +18,10 @@ class UserPrivilegeTest extends TestCase
     public function canSellerUpgradeOrDowngradePlanData()
     {
         return array(
-            array('test', 7, 1, false), //Invalid user id and valid plan id            
             array('test', 'test', 1, false), //Invalid plan id and user id
+            array('test', 7, 1, false), //Invalid user id but valid plan id            
+            array(79, 'test', 1, false), //Invalid plan id but Valid user id
             array(79, 7, 1, true), //Valid user id and plan id
-            array(79, 'test', 1, false), //Invalid plan id and Valid user id
         ); 
     }
     
