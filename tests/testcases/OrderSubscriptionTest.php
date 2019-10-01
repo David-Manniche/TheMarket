023a58803d0a6536e97311c6aa8b313e3136d2d8
Tanima Bansal <tanima.bansal@maxixx.com>
2019-10-01 18:44:34 +0530

    task-66702-test cases for functions of subscription packages and vendor selected subscriptions

---
+++ tests/testcases/OrderSubscriptionTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class OrderSubscriptionTest extends TestCase
+{   
+   
+    /**
+     * @dataProvider setDataFreeSubscription
+     */
+    public function testCanUserBuyFreeSubscription($langId, $userId, $expected)
+    {
+        $result = OrderSubscription::canUserBuyFreeSubscription($langId, $userId);                                           
+        $this->assertEquals($expected, $result);
+    }
+    
+    public function setDataFreeSubscription()
+    {
+        return array(
+            array('test', 'test', true), // Invalid langid and userid
+            array('test', 80, false), // Invalid langid and userid having subscription already
+            array('test', 79, true), // Invalid langid and userid do not have subscription
+            array(1, 80, false), // Valid langid and userid having subscription already
+            array(1, 79, true), // Valid langid and userid do not have subscription
+        ); 
+    }    
+
+    
+}
