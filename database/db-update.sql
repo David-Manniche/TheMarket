4cd2d4610c4b848cbb6755cbc6db206c8c2182f6
Satbir <satbir.kaushik@fatbit.in>
2019-12-04 16:16:30 +0530

    task-68898-Table creation for custom push notifications

---
+++ database/db-update.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `tbl_custom_notifications`(
+    `cnotification_id` INT NOT NULL AUTO_INCREMENT,
+    `cnotification_type` TINYINT(1) NOT NULL,
+    `cnotification_title` VARCHAR(200) NOT NULL,
+    `cnotification_description` VARCHAR(255) NOT NULL,
+    `cnotification_notified_on` DATETIME NOT NULL,
+    `cnotification_for_buyer` TINYINT(1) NOT NULL,
+    `cnotification_for_seller` TINYINT(1) NOT NULL,
+    `cnotification_active` TINYINT(1) NOT NULL,
+    `cnotification_added_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(`cnotification_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `tbl_custom_notification_to_users` ( `cntu_cnotification_id` INT NOT NULL ,  `cntu_user_id` INT NOT NULL ) ENGINE = InnoDB;
+ALTER TABLE  `tbl_custom_notification_to_users` ADD PRIMARY KEY (`cntu_cnotification_id`, `cntu_user_id`);
