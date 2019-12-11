93be7eec3b40237c40e345f3e055cee62b99a31a
Satbir <satbir.kaushik@fatbit.in>
2019-12-11 18:34:55 +0530

    task-68804-Remove unwanted Code and Variables from Apple Sign In Plugin

--- application/controllers/AppleLoginController.php
+++ application/controllers/AppleLoginController.php
@@ -4,10 +4,6 @@ class AppleLoginController extends SocialMediaController
     private const PRODUCTION_URL = 'https://appleid.apple.com/auth/';
     public const KEY_NAME = 'AppleLogin';
 
-    private $email;
-    private $isPrivateEmailId;
-    private $appleId;
-
     public function __construct($action)
     {
         parent::__construct($action);
@@ -29,47 +25,38 @@ class AppleLoginController extends SocialMediaController
         ]);
     }
 
-    private function validateResponse($appleResponse)
-    {
-        if (false ===  MOBILE_APP_API_CALL && $_SESSION['appleSignIn']['state'] != $appleResponse['state']) {
-            $message = 'Authorization server returned an invalid state parameter';
-            $this->setErrorMessage($message);
-        }
-        if (isset($_REQUEST['error'])) {
-            $message = 'Authorization server returned an error: ' . htmlspecialchars($_REQUEST['error']);
-            $this->setErrorMessage($message);
-        }
-        $claims = explode('.', $appleResponse['id_token'])[1];
-        $claims = json_decode(base64_decode($claims), true);
-        
-        $appleUserInfo = isset($appleResponse['user']) ? json_decode($appleResponse['user'], true) : false;
-        $this->isPrivateEmailId = false;
-        if (isset($claims['is_private_email']) && $claims['is_private_email'] == true ) {
-            $this->isPrivateEmailId = true;
-        }
-
-        $this->appleId = isset($claims['sub']) ? $claims['sub'] : '';
-
-        if (false === $appleUserInfo) {
-            if (!isset($claims['email'])) {
-                $message = Labels::getLabel('MSG_UNABLE_TO_FETCH_USER_INFO', $this->siteLangId);
-                Message::addErrorMessage($message);
-                FatApp::redirectUser(CommonHelper::generateUrl());
-            }
-            $this->email = $claims['email'];
-        } else {
-            $this->email = $appleUserInfo['email'];
-        }
-    }
-
     public function index()
     {
         $post = FatApp::getPostedData();
         $userType = FatApp::getPostedData('type', FatUtility::VAR_INT, User::USER_TYPE_BUYER);
 
         if (isset($post['id_token'])) {
-            $this->validateResponse($post);
-            $userInfo = $this->doLogin($this->email, $this->appleId, $userType);
+            if (false ===  MOBILE_APP_API_CALL && $_SESSION['appleSignIn']['state'] != $post['state']) {
+                $message = 'Authorization server returned an invalid state parameter';
+                $this->setErrorMessage($message);
+            }
+            if (isset($_REQUEST['error'])) {
+                $message = 'Authorization server returned an error: ' . htmlspecialchars($_REQUEST['error']);
+                $this->setErrorMessage($message);
+            }
+            $claims = explode('.', $post['id_token'])[1];
+            $claims = json_decode(base64_decode($claims), true);
+            
+            $appleUserInfo = isset($post['user']) ? json_decode($post['user'], true) : false;
+    
+            $appleId = isset($claims['sub']) ? $claims['sub'] : '';
+    
+            if (false === $appleUserInfo) {
+                if (!isset($claims['email'])) {
+                    $message = Labels::getLabel('MSG_UNABLE_TO_FETCH_USER_INFO', $this->siteLangId);
+                    $this->setErrorMessage($message);
+                }
+                $email = $claims['email'];
+            } else {
+                $email = $appleUserInfo['email'];
+            }
+
+            $this->doLogin($email, $appleId, $userType);
         }
 
         $this->redirectAndAuthenticateUser(static::getRequestUri());
