0a651a8bc191a99729be52bae42f7fd06c374cc8
manpreet kaur <manpreet.kaur@fatbit.in>
2020-01-04 13:01:08 +0530

    task-68374-added cropper in brands and brand request media

--- admin-application/views/brands/media.php
+++ admin-application/views/brands/media.php
@@ -1,12 +1,13 @@
-<?php
-defined('SYSTEM_INIT') or die('Invalid Usage.');
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 $brandLogoFrm->setFormTagAttribute('class', 'web_form form_horizontal');
 $brandLogoFrm->developerTags['colClassPrefix'] = 'col-md-';
 $brandLogoFrm->developerTags['fld_default_col'] = 12;
+$ratioFld = $brandLogoFrm->getField('ratio_type');
+$ratioFld->addFieldTagAttribute('class', 'prefRatio-js');
 $logoFld = $brandLogoFrm->getField('logo');
 $logoFld->addFieldTagAttribute('class', 'btn btn--primary btn--sm');
-$idFld = $brandLogoFrm->getField('brand_id');
-$idFld->addFieldTagAttribute('id', 'id-js');
+$logoFld->addFieldTagAttribute('id', 'inputBrandLogoImage');
+$logoFld->addFieldTagAttribute('onClick', 'logoPopupImage(this)');
 $logoLangFld = $brandLogoFrm->getField('lang_id');
 $logoLangFld->addFieldTagAttribute('class', 'logo-language-js');
 $logoPreferredDimensions = '<small class="text--small">'.sprintf(Labels::getLabel('LBL_Preferred_Dimensions', $adminLangId), '500*500').'</small>';
@@ -19,24 +20,24 @@ $brandImageFrm->developerTags['colClassPrefix'] = 'col-md-';
 $brandImageFrm->developerTags['fld_default_col'] = 12;
 $imageFld = $brandImageFrm->getField('image');
 $imageFld->addFieldTagAttribute('class', 'btn btn--primary btn--sm');
-$idFld = $brandImageFrm->getField('brand_id');
-$idFld->addFieldTagAttribute('id', 'id-js');
+$imageFld->addFieldTagAttribute('id', 'inputbrandBannerImage');
+$imageFld->addFieldTagAttribute('onClick', 'bannerPopupImage(this)');
 $imageLangFld = $brandImageFrm->getField('lang_id');
 $imageLangFld->addFieldTagAttribute('class', 'image-language-js');
 $screenFld = $brandImageFrm->getField('slide_screen');
 $screenFld->addFieldTagAttribute('class', 'prefDimensions-js');
 
-$htmlAfterField = '<div style="margin-top:15px;" class="preferredDimensions-js">'.sprintf(Labels::getLabel('LBL_Preferred_Dimensions_%s',$adminLangId),'2000 x 500').'</div>';
+$htmlAfterField = '<div style="margin-top:15px;" class="preferredDimensions-js">'.sprintf(Labels::getLabel('LBL_Preferred_Dimensions_%s', $adminLangId), '2000 x 500').'</div>';
 $htmlAfterField .= '<div id="image-listing"></div>';
 $imageFld->htmlAfterField = $htmlAfterField;
 
 /*$ImagePreferredDimensions = '<small class="text--small">'.sprintf(Labels::getLabel('LBL_Preferred_Dimensions', $adminLangId), '2000*500').'<br/>'. Labels::getLabel('LBL_This_image_will_be_displayed_for_homepage_brands_collection', $adminLangId) .'</small>';
 $htmlAfterField = $ImagePreferredDimensions;
 $htmlAfterField .= '<div id="image-listing"></div>';
-$imageFld->htmlAfterField = $htmlAfterField;*/
-?><section class="section">
+$imageFld->htmlAfterField = $htmlAfterField;*/ ?>
+<div id="cropperBox-js"></div>
+<section class="section" id="mediaForm-js">
     <div class="sectionhead">
-
         <h4><?php echo Labels::getLabel('LBL_Product_Brand_Setup', $adminLangId); ?></h4>
     </div>
     <div class="sectionbody space">
@@ -71,20 +72,48 @@ $imageFld->htmlAfterField = $htmlAfterField;*/
     </div>
 </section>
 <script>
-    $(document).on('change','.prefDimensions-js',function(){
-        var screenDesktop = <?php echo applicationConstants::SCREEN_DESKTOP ?>;
-        var screenIpad = <?php echo applicationConstants::SCREEN_IPAD ?>;
+$('input[name=banner_min_width]').val(2000);
+$('input[name=banner_min_height]').val(500);
+$('input[name=logo_min_width]').val(150);
+$('input[name=logo_min_height]').val(150);
+var ratioTypeSquare = <?php echo AttachedFile::RATIO_TYPE_SQUARE; ?>;
+var ratioTypeRectangular = <?php echo AttachedFile::RATIO_TYPE_RECTANGULAR; ?>;
+var aspectRatio = 4 / 1;
 
-        if($(this).val() == screenDesktop)
-        {
-            $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '2000 x 500'));
-        }
-        else if($(this).val() == screenIpad)
-        {
-            $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '1024 x 360'));
-        }
-        else{
-            $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '640 x 360'));
-        }
-    });
+$(document).on('change','.prefDimensions-js',function(){
+    var screenDesktop = <?php echo applicationConstants::SCREEN_DESKTOP ?>;
+    var screenIpad = <?php echo applicationConstants::SCREEN_IPAD ?>;
+
+    if($(this).val() == screenDesktop)
+    {
+        $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '2000 x 500'));
+        $('input[name=banner_min_width]').val(2000);
+        $('input[name=banner_min_height]').val(500);
+        aspectRatio = 4 / 1;
+    }
+    else if($(this).val() == screenIpad)
+    {
+        $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '1024 x 360'));
+        $('input[name=banner_min_width]').val(1024);
+        $('input[name=banner_min_height]').val(360);
+        aspectRatio = 128 / 45;
+    }
+    else{
+        $('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '640 x 360'));
+        $('input[name=banner_min_width]').val(640);
+        $('input[name=banner_min_height]').val(360);
+        aspectRatio = 16 / 9;
+    }
+});
+
+$(document).on('change','.prefRatio-js',function(){
+    if($(this).val() == ratioTypeSquare)
+    {
+        $('input[name=logo_min_width]').val(150);
+        $('input[name=logo_min_height]').val(150);
+    } else {
+        $('input[name=logo_min_width]').val(150);
+        $('input[name=logo_min_height]').val(85);
+    }
+});
 </script>
--- admin-application/views/brands/page-js/brand-requests.js
+++ admin-application/views/brands/page-js/brand-requests.js
@@ -21,16 +21,22 @@ $(document).ready(function(){
 		}
 	});
 });
-$(document).on('change','.language-js',function(){
-/* $(document).delegate('.language-js','change',function(){ */
+$(document).on('change','.logo-language-js',function(){
 	var lang_id = $(this).val();
-	var brand_id = $("input[id='id-js']").val();
-	brandImages(brand_id, 'logo', lang_id);
+	var brand_id = $(this).closest("form").find('input[name="brand_id"]').val();
+	brandImages(brand_id, 'logo', 0, lang_id);
 });
-$(document).delegate('.image-language-js','change',function(){
+$(document).on('change','.image-language-js',function(){
 	var lang_id = $(this).val();
-	var brand_id = $("input[id='id-js']").val();
-	brandImages(brand_id, 'image', lang_id);
+	var brand_id = $(this).closest("form").find('input[name="brand_id"]').val();
+	var slide_screen = $(".prefDimensions-js").val();
+	brandImages(brand_id, 'image', slide_screen, lang_id);
+});
+$(document).on('change','.prefDimensions-js',function(){
+	var slide_screen = $(this).val();
+	var brand_id = $(this).closest("form").find('input[name="brand_id"]').val();
+	var lang_id = $(".image-language-js").val();
+	brandImages(brand_id, 'image', slide_screen, lang_id);
 });
 (function() {
 	var currentPage = 1;
@@ -109,14 +115,14 @@ $(document).delegate('.image-language-js','change',function(){
     brandRequestMediaForm = function(brandId){
 		fcom.displayProcessing();
         fcom.ajax(fcom.makeUrl('Brands', 'requestMedia', [brandId]), '', function(t) {
-            brandImages(brandId, 'logo');
-            brandImages(brandId, 'image');
+            brandImages(brandId, 'logo', 1);
+            brandImages(brandId, 'image', 1);
             fcom.updateFaceboxContent(t);
         });
 	};
 
-	brandImages = function(brandId, fileType, langId){
-		fcom.ajax(fcom.makeUrl('Brands', 'images', [brandId, fileType, langId]), '', function(t) {
+	brandImages = function(brandId, fileType, slide_screen, langId){
+		fcom.ajax(fcom.makeUrl('Brands', 'images', [brandId, fileType, langId, slide_screen]), '', function(t) {
 			if(fileType=='logo') {
 				$('#logo-listing').html(t);
 			} else {
@@ -125,7 +131,6 @@ $(document).delegate('.image-language-js','change',function(){
 			fcom.resetFaceboxHeight();
 		});
 	};
-
 
 	deleteRecord = function(id){
 		if(!confirm(langLbl.confirmDelete)){return;}
@@ -140,10 +145,10 @@ $(document).delegate('.image-language-js','change',function(){
 		searchProductBrands(document.frmSearch);
 	};
 
-	deleteMedia = function( brandId, fileType, langId ){
+	deleteMedia = function( brandId, fileType, langId, slide_screen ){
 		if(!confirm(langLbl.confirmDelete)){return;}
-		fcom.updateWithAjax(fcom.makeUrl('brands', 'removeBrandMedia',[brandId, fileType, langId]), '', function(t) {
-			brandImages(brandId,fileType,langId);
+		fcom.updateWithAjax(fcom.makeUrl('brands', 'removeBrandMedia',[brandId, fileType, langId, slide_screen]), '', function(t) {
+			brandImages(brandId,fileType,slide_screen,langId);
 			reloadList();
 		});
 	};
@@ -170,72 +175,110 @@ $(document).delegate('.image-language-js','change',function(){
 		}
 	};
 
+	bannerPopupImage = function(inputBtn){
+        fcom.ajax(fcom.makeUrl('Brands', 'imgCropper'), '', function(t) {
+			$('#cropperBox-js').html(t);
+			$("#mediaForm-js").css("display", "none");
+    		var container = document.querySelector('.img-container');
+    		var image = container.getElementsByTagName('img').item(0);
+            var minWidth = document.frmBrandImage.banner_min_width.value;
+            var minHeight = document.frmBrandImage.banner_min_height.value;
+    		var options = {
+                aspectRatio: aspectRatio,
+                data: {
+                    width: minWidth,
+                    height: minHeight,
+                },
+                minCropBoxWidth: minWidth,
+                minCropBoxHeight: minHeight,
+                toggleDragModeOnDblclick: false,
+	        };
+    	  return cropImage(image, options, 'uploadBrandImages', inputBtn);
+    	});
+	};
+
+    logoPopupImage = function(inputBtn){
+        fcom.ajax(fcom.makeUrl('Brands', 'imgCropper'), '', function(t) {
+			$('#cropperBox-js').html(t);
+			$("#mediaForm-js").css("display", "none");
+    		var container = document.querySelector('.img-container');
+    		var image = container.getElementsByTagName('img').item(0);
+            var minWidth = document.frmBrandLogo.logo_min_width.value;
+            var minHeight = document.frmBrandLogo.logo_min_height.value;
+			if(minWidth == minHeight){
+				var aspectRatio = 1 / 1
+			} else {
+                var aspectRatio = 16 / 9;
+            }
+    		var options = {
+                aspectRatio: aspectRatio,
+                data: {
+                    width: minWidth,
+                    height: minHeight,
+                },
+                minCropBoxWidth: minWidth,
+                minCropBoxHeight: minHeight,
+                toggleDragModeOnDblclick: false,
+	        };
+    	  return cropImage(image, options, 'uploadBrandImages', inputBtn);
+    	});
+	};
+
+	uploadBrandImages = function(formData){
+        var node = this;
+        $('#form-upload').remove();
+        var frmName = formData.get("frmName");
+        if ('frmBrandLogo' == frmName) {
+			var brandId = document.frmBrandLogo.brand_id.value;
+            var langId = document.frmBrandLogo.lang_id.value;
+            var fileType = document.frmBrandLogo.file_type.value;
+            var imageType = 'logo';
+        } else {
+			var brandId = document.frmBrandImage.brand_id.value;
+            var langId = document.frmBrandImage.lang_id.value;
+            var slideScreen = document.frmBrandImage.slide_screen.value;
+            var fileType = document.frmBrandImage.file_type.value;
+            var imageType = 'banner';
+        }
+
+		formData.append('brand_id', brandId);
+        formData.append('slide_screen', slideScreen);
+        formData.append('lang_id', langId);
+        formData.append('file_type', fileType);
+        /* $val = $(node).val(); */
+        $.ajax({
+            url: fcom.makeUrl('Brands', 'uploadMedia'),
+            type: 'post',
+            dataType: 'json',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $(node).val('Loading');
+            },
+            complete: function() {
+                /* $(node).val($val); */
+            },
+			success: function(ans) {
+				$('.text-danger').remove();
+				$('#input-field').html(ans.msg);
+				if( ans.status == true ){
+					$('#input-field').removeClass('text-danger');
+					$('#input-field').addClass('text-success');
+					$('#form-upload').remove();
+					brandRequestMediaForm(ans.brandId);
+					brandImages(ans.brandId,imageType,langId);
+				}else{
+					$('#input-field').removeClass('text-success');
+					$('#input-field').addClass('text-danger');
+				}
+				reloadList();
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+			}
+        });
+	}
+
 })();
-
-$(document).on('click','.uploadFile-Js',function(){
-	var node = this;
-	$('#form-upload').remove();
-
-	var formName = $(node).attr('data-frm');
-	if(formName == 'frmBrandImage'){
-        var brandId = document.frmBrandImage.brand_id.value;
-        var langId = document.frmBrandImage.lang_id.value;
-        var imageType = 'image';
-	}else{
-		var brandId = document.frmBrandLogo.brand_id.value;
-        var langId = document.frmBrandLogo.lang_id.value;
-		var imageType = 'logo';
-	}
-
-    var fileType = $(node).attr('data-file_type');
-
-	var frm = '<form enctype="multipart/form-data" id="form-upload" style="position:absolute; top:-100px;" >';
-	frm = frm.concat('<input type="file" name="file" />');
-	frm = frm.concat('<input type="hidden" name="brand_id" value="' + brandId + '"/>');
-	frm = frm.concat('<input type="hidden" name="lang_id" value="' + langId + '"/>');
-    frm = frm.concat('<input type="hidden" name="file_type" value="' + fileType + '">');
-	frm = frm.concat('</form>');
-	$( 'body' ).prepend( frm );
-	$('#form-upload input[name=\'file\']').trigger('click');
-	if ( typeof timer != 'undefined' ) {
-		clearInterval(timer);
-	}
-	timer = setInterval(function() {
-		if ($('#form-upload input[name=\'file\']').val() != '') {
-			clearInterval(timer);
-			$val = $(node).val();
-			$.ajax({
-				url: fcom.makeUrl('Brands', 'uploadMedia'),
-				type: 'post',
-				dataType: 'json',
-				data: new FormData($('#form-upload')[0]),
-				cache: false,
-				contentType: false,
-				processData: false,
-				beforeSend: function() {
-					$(node).val('Loading');
-				},
-				complete: function() {
-					$(node).val($val);
-				},
-				success: function(ans) {
-						$('.text-danger').remove();
-						$('#input-field').html(ans.msg);
-						if( ans.status == true ){
-							$('#input-field').removeClass('text-danger');
-							$('#input-field').addClass('text-success');
-							$('#form-upload').remove();
-							brandImages(ans.brandId,imageType,langId);
-						}else{
-							$('#input-field').removeClass('text-success');
-							$('#input-field').addClass('text-danger');
-						}
-						reloadList();
-					},
-					error: function(xhr, ajaxOptions, thrownError) {
-						alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-					}
-				});
-		}
-	}, 500);
-});
