9690888b03550b5f6ace1b10b81f1f363256e21e
Anup Rawat <anup.rawat@ablysoft.com>
2020-01-08 15:35:23 +0530

    task-66015 auto restore popup auto close

--- application/views/restore-system/page-content.php
+++ application/views/restore-system/page-content.php
@@ -46,24 +46,28 @@
         if (distance < 0) {           
             clearInterval(x);
             $('#restoreCounter').html("Process...");
-            showRestorePopup();
+            showRestorePopup(true);
             restoreSystem();
         }
     }, 1000);
 
-    function showRestorePopup() {
+    function showRestorePopup(autoClose) {
         $.facebox(
             '<div class="demo-data-inner"><div class="heading">Yo!kart<span></span></div> <p>To enhance your demo experience, we periodically  restore our database every 4 hours.</p><br> <p>For technical issues :-</p> <ul> <li><strong>Call us at: </strong>+1 469 844 3346, +91 85919 19191, +91 95555 96666, +91 73075 70707, +91 93565 35757</li> <li><strong>Mail us at : </strong> <a href="mailto:sales@fatbit.com">sales@fatbit.com</a></li> </ul> <br> Create Your Dream Multi-vendor Ecommerce Store With Yo!Kart <a href="https://www.yo-kart.com/contact-us.html" target="_blank">Click here</a></li></div>',
             'restore-demo restore-demo-bg');
+        if(typeof autoClose !== 'undefined' && autoClose == true){    
+            setTimeout(function() {window.location.reload(true);}, 5000); 
+        }               
     }
 
     function restoreSystem() {
         $.mbsmessage('Restore is in process..', false, 'alert--process alert');
+        setTimeout(function() {$.facebox.close();}, 5000);
         fcom.updateWithAjax(fcom.makeUrl('RestoreSystem', 'index', '',
             '<?php echo CONF_WEBROOT_FRONT_URL;?>'), '', function(
             resp) {
             setTimeout(function() {
-                window.location.reload();
+                window.location.reload(true);
             }, 5000);
         }, false, false);
     }
