13dfe18c55b3ed56aba6a76d244f5c7e231fca0a
Anup Rawat <anup.rawat@ablysoft.com>
2020-01-08 17:52:03 +0530

    task-66015 category page banner listing

--- application/views/products/listing-page.php
+++ application/views/products/listing-page.php
@@ -1,4 +1,4 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.'); 
 if (empty($products)) {
     $pSrchFrm = Common::getSiteSearchForm();
     $pSrchFrm->fill(array('btnSiteSrchSubmit' => Labels::getLabel('LBL_Submit', $siteLangId)));
@@ -17,7 +17,7 @@ $desktop_url = '';
 $tablet_url = '';
 $mobile_url = '';
 $category['banner'] = isset($category['banner']) ? (array) $category['banner'] : array();
-if (!empty($category['banner'])) {
+if (!empty($category['banner'])) { 
     $catBannerArr = AttachedFile::getMultipleAttachments(AttachedFile::FILETYPE_CATEGORY_BANNER, $category['prodcat_id'], 0, $siteLangId);
     foreach ($catBannerArr as $slideScreen) {
         $uploadedTime = AttachedFile::setTimeParam($slideScreen['afile_updated_at']);
@@ -32,7 +32,7 @@ if (!empty($category['banner'])) {
                 $desktop_url = ' >1025:' .FatCache::getCachedUrl(CommonHelper::generateUrl('Category', 'Banner', array($category['prodcat_id'], $siteLangId, 'DESKTOP', applicationConstants::SCREEN_DESKTOP)).$uploadedTime, CONF_IMG_CACHE_TIME, '.jpg').",";
                 break;
             } ?>
-
+    <?php } ?>
     <section class="bg-shop">
         <div class="shop-banner"><img data-ratio="4:1" data-src-base="" data-src-base2x="" data-src="<?php echo $mobile_url . $tablet_url  . $desktop_url; ?>"
                 src="<?php echo CommonHelper::generateUrl('Category', 'Banner', array($category['prodcat_id'],$siteLangId,'DESKTOP',applicationConstants::SCREEN_DESKTOP)); ?>"></div>
@@ -41,8 +41,7 @@ if (!empty($category['banner'])) {
             <p><?php  echo FatUtility::decodeHtmlEntities($category['prodcat_description']); ?></p>
         </div>
         <?php } */ ?>
-    </section>
-    <?php } ?>
+    </section>    
 <?php }
 if (array_key_exists('brand_id', $postedData) && $postedData['brand_id'] > 0) {
     $brandImgArr = AttachedFile::getMultipleAttachments(AttachedFile::FILETYPE_BRAND_IMAGE, $postedData['brand_id'], 0, $siteLangId);
