7abb7d8525b7a128d606c891a6f86fedd32dae0c
ablydzpawan <pawan.kumar.dz@ablysoft.com>
2020-02-17 11:44:11 +0530

    task-69737- fixed icons for admin

--- admin-application/views/products/index.php
+++ admin-application/views/products/index.php
@@ -32,9 +32,9 @@
                         <?php
                             if ($canEdit) {
                                 $div = new HtmlElement("div", array("class"=>"section__toolbar"));
-                                $div->appendElement('a', array('href'=>'javascript:void(0)','class'=>'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none','title'=>Labels::getLabel('LBL_Publish', $adminLangId),"onclick"=>"toggleBulkStatues(1)"), '<i class="fas fa-plus"></i>', true);
+                                $div->appendElement('a', array('href'=>'javascript:void(0)','class'=>'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none','title'=>Labels::getLabel('LBL_Publish', $adminLangId),"onclick"=>"toggleBulkStatues(1)"), '<i class="fas fa-eye"></i>', true);
                                 
-                                $div->appendElement('a', array('href'=>'javascript:void(0)','class'=>'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none','title'=>Labels::getLabel('LBL_Unpublish', $adminLangId),"onclick"=>"toggleBulkStatues(0)"), '<i class="fas fa-plus"></i>', true);
+                                $div->appendElement('a', array('href'=>'javascript:void(0)','class'=>'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none','title'=>Labels::getLabel('LBL_Unpublish', $adminLangId),"onclick"=>"toggleBulkStatues(0)"), '<i class="fas fa-eye-slash"></i>', true);
                                 
                                 $div->appendElement('a', array('href'=>'javascript:void(0)','class'=>'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none','title'=>Labels::getLabel('LBL_Delete', $adminLangId),"onclick"=>"deleteSelected()"), '<i class="fas fa-trash"></i>', true);
                                 
