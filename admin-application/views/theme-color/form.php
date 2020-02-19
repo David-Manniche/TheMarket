62140ae359ac52808fdf0a8fecaa37da3ffdff5f
manpreet kaur <manpreet.kaur@fatbit.in>
2020-02-19 18:29:35 +0530

    bug-034230-worked on theme settings updates

--- admin-application/views/theme-color/form.php
+++ admin-application/views/theme-color/form.php
@@ -3,33 +3,22 @@ $frm->setFormTagAttribute('class', 'web_form form_horizontal');
 $frm->setFormTagAttribute('onsubmit', 'setupThemeColor(this); return(false);');
 $frm->developerTags['colClassPrefix'] = 'col-md-';
 $frm->developerTags['fld_default_col'] = 12;
-
 ?>
-
-
 <section class="section">
-	<div class="sectionhead">
-		<h4><?php echo Labels::getLabel('LBL_Theme_Color_Setup',$adminLangId); ?></h4>
-	</div>
-	<div class="sectionbody space">
-		<div class="row">		
-			<div class="col-sm-12">
-				<div class="tabs_nav_container responsive flat">
-					<ul class="tabs_nav">
-						<li><a class="active" href="javascript:void(0)" onclick="editThemeColorForm(<?php echo $tcolor_id ?>);"><?php echo Labels::getLabel('LBL_General',$adminLangId); ?></a></li>
-                        <li class="<?php echo (0 == $tcolor_id) ? 'fat-inactive' : ''; ?>">
-                            <a href="javascript:void(0);" <?php echo (0 < $tcolor_id) ? "onclick='editThemeColorLangForm(" . $tcolor_id . "," . FatApp::getConfig('conf_default_site_lang', FatUtility::VAR_INT, 1) . ");'" : ""; ?>>
-                                <?php echo Labels::getLabel('LBL_Language_Data', $adminLangId); ?>
-                            </a>
-                        </li>
-					</ul>
-					<div class="tabs_panel_wrap">
-						<div class="tabs_panel">
-							<?php echo $frm->getFormHtml(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="sectionhead">
+        <h4><?php echo Labels::getLabel('LBL_Theme_Color_Setup', $adminLangId); ?></h4>
+    </div>
+    <div class="sectionbody space">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="tabs_nav_container responsive flat">
+                    <div class="tabs_panel_wrap">
+                        <div class="tabs_panel">
+                            <?php echo $frm->getFormHtml(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
