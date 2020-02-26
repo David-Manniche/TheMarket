6e584a52625a869aad10ce0adf9a7cb1d2a8c171
manpreet kaur <manpreet.kaur@fatbit.in>
2020-02-26 11:10:56 +0530

    task-70000-added download files tab in inventory setup

--- application/controllers/ShopsController.php
+++ application/controllers/ShopsController.php
@@ -332,7 +332,7 @@ class ShopsController extends MyAppController
                 $this->_template->addCss('shops/templates/page-css/'.SHOP::TEMPLATE_ONE.'.css');
             break;
         } */
-        $this->_template->addCss('shops/templates/page-css/' . SHOP::TEMPLATE_ONE . '.css');
+        /* $this->_template->addCss('shops/templates/page-css/' . SHOP::TEMPLATE_ONE . '.css'); */
         $this->set('shop', $this->shopPoliciesData($shop));
         $this->set('shopRating', SelProdRating::getSellerRating($shop['shop_user_id']));
         $this->set('shopTotalReviews', SelProdReview::getSellerTotalReviews($shop['shop_user_id']));
