227494bf850cb315fbc211e312d72983eb175f01
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-05-12 18:37:03 +0530

    task-72361-Remove unwanted js code from js file

--- application/views/stripe-connect/page-js/index.js
+++ application/views/stripe-connect/page-js/index.js
@@ -23,29 +23,4 @@ var keyName = 'StripeConnect';
             location.reload();
         });
     }
-
-    /*setupFinancialInfo = function (frm){
-		if (!$(frm).validate()) return;
-		// var dataQueryString = fcom.frmData(frm);
-        var data = $(frm).serializeArray().reduce(function(obj, item) {
-                        obj[item.name] = item.value;
-                        return obj;
-                    }, {}); 
-
-		// fcom.updateWithAjax(fcom.makeUrl(keyName, 'setupFinancialInfo'), data, function(t) {});
-        stripe.createToken('bank_account', data).then(function(result) {
-            // Handle result.error or result.token
-            console.log(result);
-            if (result.error) {
-                console.log(result.error);
-                $.mbsmessage( result.error.message, '', 'alert--danger');
-            } else {
-                var newData = "token=" . result.token;
-                fcom.updateWithAjax(fcom.makeUrl(keyName, 'setupFinancialInfo'), newData, function(t) {
-                    // $("#facebox .close").trigger('click');
-                    // location.reload();
-                });
-            }
-        });
-    }*/
 })();
