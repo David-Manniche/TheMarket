7186d618cf6bb74078372a9c364595ca450b12bd
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-08-20 10:37:41 +0530

    task-66150-Disable wallet selection in case of stripe connect

--- application/views/checkout/payment-summary.php
+++ application/views/checkout/payment-summary.php
@@ -97,23 +97,10 @@ $rewardPoints = UserRewardBreakup::rewardPointBalance(UserAuthentication::getLog
                     </ul>
                 </div>
             <?php } ?>
-
-            <?php 
-			/* if ($canUseWalletForPayment) {
-				if ($userWalletBalance > 0 && $cartSummary['orderNetAmount'] > 0) { ?>
-				<label class="checkbox"><input onChange="walletSelection(this)" type="checkbox" <?php echo ($cartSummary["cartWalletSelected"]) ? 'checked="checked"' : ''; ?> name="pay_from_wallet" id="pay_from_wallet" value="1"><?php if ($cartSummary["cartWalletSelected"]) {
-					echo ''.Labels::getLabel('MSG_Applied_Wallet_Credits', $siteLangId)?>: <?php echo CommonHelper::displayMoneyFormat($cartSummary["WalletAmountCharge"], true, false, true, false, true);
-				} else {
-					echo ''.Labels::getLabel('MSG_Apply_Wallet_Credits', $siteLangId)?>: <?php echo CommonHelper::displayMoneyFormat($userWalletBalance, true, false, true, false, true)?>
-				<?php } ?> <i class="input-helper"></i>            
-
-				</label>
-				<?php }
-			} */ ?>
         </div>
     </div>
 </main>
-<?php if ($userWalletBalance > 0 && $cartSummary['orderNetAmount'] > 0) { ?>
+<?php if ($userWalletBalance > 0 && $cartSummary['orderNetAmount'] > 0 && $canUseWalletForPayment) { ?>
     <div class="wallet-balance">
          <label class="checkbox wallet">
             <input onChange="walletSelection(this)" type="checkbox" <?php echo ($cartSummary["cartWalletSelected"]) ? 'checked="checked"' : ''; ?> name="pay_from_wallet" id="pay_from_wallet" value="1">
