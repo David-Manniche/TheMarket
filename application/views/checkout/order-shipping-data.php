216cc1482e244eb510eef26432638f3962153751
Sunil Kumar <sunil.kumar@fatbit.in>
2020-09-02 18:19:04 +0530

    task-75252-modified selected shipping data on paymnet summary page

--- application/views/checkout/order-shipping-data.php
+++ application/views/checkout/order-shipping-data.php
@@ -4,28 +4,19 @@ if(!empty($orderShippingData)){
         <div class="pop-up-title"><?php echo Labels::getLabel('LBL_Shipping', $siteLangId); ?></div>
         <div class="pick-section">
             <ul class="list-group review-block">
-                <?php foreach($orderShippingData as $key=>$data) { 
+                <?php foreach($orderShippingData as $shipData) { ?>
+                <li class="list-group-item">
+                <?php foreach($shipData as $data) { 
                     $productUrl = UrlHelper::generateUrl('Products', 'View', array($data['op_selprod_id']));
                 ?>
-                 <li class="list-group-item">
-                    <div class="review-block__content" role="cell">  
-                        <div class="delivery-address"> 
-                            <div class="product-profile">
-                                <div class="product-profile__thumbnail">
-                                    <a href="<?php echo $productUrl;?>">
-                                        <img class="img-fluid" data-ratio="3:4"
-                                            src="<?php echo UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'product', array($data['selprod_product_id'], "THUMB", $data['op_selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg'); ?>" alt="<?php echo $data['op_selprod_title']; ?>" title="<?php echo $data['op_selprod_title']; ?>">
-                                    </a>
-                                </div>
-                                <div class="product-profile__data">
-                                    <div class="title"><?php echo $data['opshipping_label']; ?></div>
-                                </div>
-                            </div>   
-                        </div>
+                    <div class="product-profile__thumbnail">
+                        <a href="<?php echo $productUrl;?>">
+                            <img class="img-fluid" data-ratio="3:4" src="<?php echo UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'product', array($data['selprod_product_id'], "THUMB", $data['op_selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg'); ?>" alt="<?php echo $data['op_selprod_title']; ?>" title="<?php echo $data['op_selprod_title']; ?>">
+                        </a>
                     </div>
-                    <div class="review-block__link" role="cell">
-                        <a class="link" href="javascript:void(0);" onClick="ShippingSummaryData();"><span><?php echo Labels::getLabel('LBL_Change_Shipping', $siteLangId); ?></span></a>
-                    </div>
+                <?php } ?>
+                    <div class="title product-profile__data"><?php echo $data['opshipping_label']; ?></div>
+                    <a class="link" href="javascript:void(0);" onClick="ShippingSummaryData();"><span><?php echo Labels::getLabel('LBL_Change_Shipping', $siteLangId); ?></span></a>
                 </li>
                 <?php } ?>
             </ul>
