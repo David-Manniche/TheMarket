b4854d6d5b3b770998b3d292907052e06a692f50
Anup Rawat <anup.rawat@ablysoft.com>
2020-10-13 15:54:36 +0530

    bug-043719 shipping profile issue on checkout

--- application/mobile-views/checkout/fulfillment-ship-summary.php
+++ application/mobile-views/checkout/fulfillment-ship-summary.php
@@ -1,119 +1,124 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
-
-if (array_key_exists(Shipping::BY_ADMIN, $shippingRates)) {
-    ksort($shippingRates);
-}
+ksort($shippingRates);
 $productItems[$count] = [];
-foreach ($shippingRates as $level => $levelItems) {
-    if (isset($levelItems['products']) && count($levelItems['products']) > 1 && $level != Shipping::LEVEL_PRODUCT) {
-        $productData = current($levelItems['products']);
-        $productItems[$count]['title'] = ($level == Shipping::LEVEL_SHOP) ? $productData['shop_name'] : FatApp::getConfig('CONF_WEBSITE_NAME_' . $siteLangId, null, '');
+foreach ($shippingRates as $shippedBy => $shippedByItemArr) {
+    ksort($shippedByItemArr);
+    foreach ($shippedByItemArr as $shipLevel => $items) {
+        switch ($shipLevel) {
+            case Shipping::LEVEL_ORDER:
+            case Shipping::LEVEL_SHOP:
+                if (isset($items['products']) && !empty($items['products'])) {
+                    $productData = $items['products'];
+                    $productInfo = current($productData);
 
-        if ($level != Shipping::LEVEL_PRODUCT) {
-            if (count($levelItems['rates']) > 0) {
-                $productItems[$count]['rates']['code'] = current($levelItems['rates'])['code'];
-                foreach ($levelItems['rates'] as $key => $shippingRate) {
-                    if (!empty($orderShippingData)) {
-                        foreach ($orderShippingData as $shipdata) {
-                            if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
-                                $productItems[$count]['rates']['selected'] = $key;
-                                break;
-                            }
-                        }
+                    $productItems[$count]['title'] = ($shipLevel == Shipping::LEVEL_SHOP) ? $productInfo['shop_name'] : FatApp::getConfig('CONF_WEBSITE_NAME_' . $siteLangId, null, '');
+
+                    $shippingCharges = [];
+                    if (isset($shippedByItemArr[$shipLevel]['rates'])) {
+                        $shippingCharges = $shippedByItemArr[$shipLevel]['rates'];
                     }
-                    $productItems[$count]['rates']['data'][] = [
-                        'title' => $shippingRate['title'],
-                        'cost' => $shippingRate['cost'],
-                        'id' => $shippingRate['id'],
-                    ];
-                }
-            } else {
-                $productItems[$count]['rates']['data'] = [];
-            }
-        }
-    }
 
-    if (isset($levelItems['products'])) {
-        foreach ($levelItems['products'] as $product) {
-            if (count($levelItems['products']) == 1 && count($levelItems['rates']) > 0 && $level != Shipping::LEVEL_PRODUCT) {
-                $productItems[$count]['title'] = $product['shop_name'];
-                if (count($levelItems['rates']) > 0) {
-                    $productItems[$count]['rates']['code'] =  current($levelItems['rates'])['code'];
-
-                    foreach ($levelItems['rates'] as $key => $shippingRate) {
-                        if (!empty($orderShippingData)) {
-                            foreach ($orderShippingData as $shipdata) {
-                                if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
-                                    $productItems[$count]['rates']['selected'] = $key;
-                                    break;
+                    if (count($shippingCharges) > 0) {
+                        $name = current($shippingCharges)['code'];
+                        $productItems[$count]['rates']['code'] =  $name;
+                        foreach ($shippingCharges as $key => $shippingcharge) {
+                            if (!empty($orderShippingData)) {
+                                foreach ($orderShippingData as $shipdata) {
+                                    if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
+                                        $productItems[$count]['rates']['selected'] = $key;
+                                        break;
+                                    }
                                 }
                             }
+
+                            $productItems[$count]['rates']['data'][] = [
+                                'title' => $shippingcharge['title'],
+                                'cost' => $shippingcharge['cost'],
+                                'id' => $shippingcharge['id'],
+                            ];
                         }
-
-                        $productItems[$count]['rates']['data'][] = [
-                            'title' => $shippingRate['title'],
-                            'cost' => $shippingRate['cost'],
-                            'id' => $shippingRate['id'],
-                        ];
+                    } else {
+                        $productItems[$count]['rates']['data'] = [];
                     }
-                } elseif ($product['product_type'] == Product::PRODUCT_TYPE_PHYSICAL) {
-                    $productItems[$count]['rates']['data'] = [];
-                }
-            }
 
-            if ($level == Shipping::LEVEL_PRODUCT && isset($levelItems['rates'][$product['selprod_id']])) {
-                if (!empty($productItems[$count])) {
+                    foreach ($productData as $product) {
+                        $product['productUrl'] = UrlHelper::generateFullUrl('Products', 'View', array($product['selprod_id']));
+                        $product['shopUrl'] = UrlHelper::generateFullUrl('Shops', 'View', array($product['shop_id']));
+                        $product['imageUrl'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], "THUMB", $product['selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg');
+                        $productItems[$count]['products'][] = $product;
+                    }
                     $count++;
                 }
-
-                $productItems[$count]['title'] = $product['shop_name'];
-
-                $priceListCount = count($levelItems['rates'][$product['selprod_id']]);
-                if ($priceListCount > 0) {
-                    $productItems[$count]['rates']['code'] =  current($levelItems['rates'][$product['selprod_id']])['code'];
-
-                    foreach ($levelItems['rates'][$product['selprod_id']] as $key => $shippingRate) {
-                        if (!empty($orderShippingData)) {
-                            foreach ($orderShippingData as $shipdata) {
-                                if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
-                                    $productItems[$count]['rates']['selected'] = $key;
-                                    break;
+                break;
+            case Shipping::LEVEL_PRODUCT:
+                if (isset($items['products']) && !empty($items['products'])) {
+                    foreach ($items['products'] as $selProdid => $product) {
+                        $productItems[$count]['title'] = $product['shop_name'];
+                        $priceListCount = count($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']]);
+                        if ($priceListCount > 0) {
+                            $name = current($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']])['code'];
+                            $productItems[$count]['rates']['code'] =  $name;
+                            foreach ($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']] as $key => $shippingcharge) {
+                                if (!empty($orderShippingData)) {
+                                    foreach ($orderShippingData as $shipdata) {
+                                        if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
+                                            $productItems[$count]['rates']['selected'] = $key;
+                                            break;
+                                        }
+                                    }
                                 }
+                                $productItems[$count]['rates']['data'][] = [
+                                    'title' => $shippingcharge['title'],
+                                    'cost' => $shippingcharge['cost'],
+                                    'id' => $shippingcharge['id'],
+                                ];
                             }
+                        } else {
+                            $productItems[$count]['rates']['data'] = [];
                         }
 
-                        $productItems[$count]['rates']['data'][] = [
-                            'title' => $shippingRate['title'],
-                            'cost' => $shippingRate['cost'],
-                            'id' => $shippingRate['id'],
-                        ];
+                        $product['productUrl'] = UrlHelper::generateFullUrl('Products', 'View', array($product['selprod_id']));
+                        $product['shopUrl'] = UrlHelper::generateFullUrl('Shops', 'View', array($product['shop_id']));
+                        $product['imageUrl'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], "THUMB", $product['selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg');
+                        $productItems[$count]['products'][] = $product;
+                        $count++;
                     }
-                } elseif ($product['product_type'] == Product::PRODUCT_TYPE_PHYSICAL) {
-                    $productItems[$count]['rates']['data'] = [];
                 }
-            }
 
-            $product['productUrl'] = UrlHelper::generateFullUrl('Products', 'View', array($product['selprod_id']));
-            $product['shopUrl'] = UrlHelper::generateFullUrl('Shops', 'View', array($product['shop_id']));
-            $product['imageUrl'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], "THUMB",$product['selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg');
-            $productItems[$count]['products'][] = $product;
+                if (isset($items['digital_products']) && !empty($items['digital_products'])) {
+                    foreach ($items['digital_products'] as $selProdid => $product) {
+                        $productItems[$count]['title'] = $product['shop_name'];
+                        $priceListCount = count($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']]);
+                        if ($priceListCount > 0) {
+                            $name = current($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']])['code'];
+                            $productItems[$count]['rates']['code'] =  $name;
+                            foreach ($shippedByItemArr[$shipLevel]['rates'][$product['selprod_id']] as $key => $shippingcharge) {
+                                if (!empty($orderShippingData)) {
+                                    foreach ($orderShippingData as $shipdata) {
+                                        if ($shipdata['opshipping_code'] == $name && ($key == $shipdata['opshipping_carrier_code'] . "|" . $shipdata['opshipping_label'] || $key == $shipdata['opshipping_rate_id'])) {
+                                            $productItems[$count]['rates']['selected'] = $key;
+                                            break;
+                                        }
+                                    }
+                                }
+                                $productItems[$count]['rates']['data'][] = [
+                                    'title' => $shippingcharge['title'],
+                                    'cost' => $shippingcharge['cost'],
+                                    'id' => $shippingcharge['id'],
+                                ];
+                            }
+                        } else {
+                            $productItems[$count]['rates']['data'] = [];
+                        }
 
-            if (isset($levelItems['products']) && count($levelItems['products']) == 1) {
-                $count++;
-            }
+                        $product['productUrl'] = UrlHelper::generateFullUrl('Products', 'View', array($product['selprod_id']));
+                        $product['shopUrl'] = UrlHelper::generateFullUrl('Shops', 'View', array($product['shop_id']));
+                        $product['imageUrl'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], "THUMB", $product['selprod_id'], 0, $siteLangId)), CONF_IMG_CACHE_TIME, '.jpg');
+                        $productItems[$count]['products'][] = $product;
+                        $count++;
+                    }
+                }
+                break;
         }
-    }
-
-    if (isset($levelItems['products']) && count($levelItems['products']) > 1) {
-        $count++;
-    }
-
-    if (isset($levelItems['digital_products']) && count($levelItems['digital_products']) > 0) {
-        foreach ($levelItems['digital_products'] as $product) {
-            $productItems[$count]['title'] = $product['shop_name'];
-            $productItems[$count]['rates']['code'] = '';
-            $productItems[$count]['products'][] = $product;
-        }
-        $count++;
     }
 }
