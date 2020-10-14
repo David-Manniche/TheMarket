ba549a1356f6a6b222f7eed3678256ddf0f7cf12
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-10-14 09:26:13 +0530

    task-66150-Db update queries updated in file

--- database/db-update.sql
+++ database/db-update.sql
@@ -803,3 +803,13 @@ ALTER TABLE `tbl_shop_specifics` ADD `shop_invoice_codes` VARCHAR(255) NOT NULL 
 -- -------------TV-9.2.1.20201008-------------------
 
 UPDATE `tbl_shops` SET `shop_fulfillment_type`=2 WHERE `shop_fulfillment_type` = 0;
+
+
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr._No';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr_no.';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr.';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr_No';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr._no.';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_SrNo.';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr._no.';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE 'LBL_Sr';
