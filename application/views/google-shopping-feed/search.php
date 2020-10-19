197a64dc21a0dc7c3a4da0b293e9a261df19b80f
Manpreet Kaur <manpreet.kaur@fatbit.in>
2020-10-19 13:15:17 +0530

    task-69144-updated url rewriting page html for pagination

--- application/views/google-shopping-feed/search.php
+++ application/views/google-shopping-feed/search.php
@@ -15,7 +15,11 @@ if (!$canEdit) {
 if (1 > count($arrListing)) {
     unset($arr_flds['select_all']);
 }
-$tbl = new HtmlElement('table', array('width' => '100%', 'class' => 'table','id' => 'plugin'));
+$tableClass = '';
+if (0 < count($arrListing)) {
+	$tableClass = "table-justified";
+}
+$tbl = new HtmlElement('table', array('width' => '100%', 'class' => 'table '.$tableClass,'id' => 'plugin'));
 $th = $tbl->appendElement('thead')->appendElement('tr');
 foreach ($arr_flds as $key => $val) {
     if ('select_all' == $key) {
