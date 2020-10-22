b17509b93364340deb0d7ea06ff1bde94c64d448
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-10-22 17:23:53 +0530

    task-70159-Slot days by addrss API response modification

--- application/mobile-views/addresses/slot-days-by-addr.php
+++ application/mobile-views/addresses/slot-days-by-addr.php
@@ -1,7 +1,7 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 $data = array(
     'slotDays' => $slotDays,
-    'activeDate' => strtotime($activeDate),
+    'activeDate' => $activeDate,
     'timeSlots' => $timeSlots,
     'selectedDate' => $selectedDate,
     'pickUpBy' => $pickUpBy,
