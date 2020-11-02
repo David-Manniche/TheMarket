4275d32b3453682101f5baf009ac1ea57b8e4344
Anup Rawat <anup.rawat@ablysoft.com>
2020-11-02 18:48:56 +0530

    task-75932 filter layout management

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -341,7 +341,7 @@ class ProductsController extends MyAppController
         }
 
         $templateName = 'filters.php';
-        if (FatApp::getConfig('CONF_FILTERS_LAYOUT', FatUtility::VAR_INT, 1) != FilterHelper::LAYOUT_TOP) {
+        if (FatApp::getConfig('CONF_FILTERS_LAYOUT', FatUtility::VAR_INT, 1) == FilterHelper::LAYOUT_TOP) {
             $templateName = 'filters-top.php';
         }
         echo $this->_template->render(false, false, 'products/' . $templateName, true);
