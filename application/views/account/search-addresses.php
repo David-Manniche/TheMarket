c8e454f866995cfa82e99430a40a051308cf59e9
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-11-03 18:08:05 +0530

    task-66150-Fix pickup addresses layout design

--- application/views/account/search-addresses.php
+++ application/views/account/search-addresses.php
@@ -4,32 +4,32 @@ if (!empty($addresses)) {
         $addresses[0]['addr_is_default'] = 1;
     } ?>
     <ul class="my-addresses">
-        <?php foreach ($addresses as $address) { 
-            $address['addr_title'] = ($address['addr_title'] == '') ? '&nbsp;' : $address['addr_title']; ?> 
-            <li class="<?php echo ($address['addr_is_default']==1)?'is-active':''; ?>">
+        <?php foreach ($addresses as $address) {
+            $address['addr_title'] = ($address['addr_title'] == '') ? '&nbsp;' : $address['addr_title']; ?>
+            <li class="<?php echo ($address['addr_is_default'] == 1) ? 'is-active' : ''; ?>">
                 <div class="my-addresses__body">
                     <span class="radio">
                         <?php
-                        $action = "setDefaultAddress(".$address['addr_id'].", event)";
+                        $action = "setDefaultAddress(" . $address['addr_id'] . ", event)";
                         if (1 == $address['addr_is_default']) {
                             $action = 'return false';
                         }
                         ?>
-                        <input type="radio" <?php echo ($address['addr_is_default']==1)?'checked=""':''; ?> name="1" onClick="<?php echo $action; ?>"><i class="input-helper"></i>
+                        <input type="radio" <?php echo ($address['addr_is_default'] == 1) ? 'checked=""' : ''; ?> name="1" onClick="<?php echo $action; ?>"><i class="input-helper"></i>
                     </span>
                     <address class="delivery-address">
                         <h5><?php echo $address['addr_name']; ?><span class="tag"><?php echo $address['addr_title']; ?></span></h5>
                         <p>
-                            <?php echo $address['addr_address1'] . '<br>'; ?> 
-                            <?php echo (strlen($address['addr_address2'])>0) ? $address['addr_address2'].'<br>':''; ?>
-                            <?php echo (strlen($address['addr_city'])>0)?$address['addr_city'].',':''; ?> 
-                            <?php echo (strlen($address['state_name'])>0)?$address['state_name'].'<br>':''; ?>
-                            <?php echo (strlen($address['country_name'])>0)?$address['country_name'].'<br>':''; ?>
-                            <?php echo (strlen($address['addr_zip'])>0) ? Labels::getLabel('LBL_Zip:', $siteLangId).$address['addr_zip'].'<br>':''; ?>
+                            <?php echo $address['addr_address1'] . '<br>'; ?>
+                            <?php echo (strlen($address['addr_address2']) > 0) ? $address['addr_address2'] . '<br>' : ''; ?>
+                            <?php echo (strlen($address['addr_city']) > 0) ? $address['addr_city'] . ',' : ''; ?>
+                            <?php echo (strlen($address['state_name']) > 0) ? $address['state_name'] . '<br>' : ''; ?>
+                            <?php echo (strlen($address['country_name']) > 0) ? $address['country_name'] . '<br>' : ''; ?>
+                            <?php echo (strlen($address['addr_zip']) > 0) ? Labels::getLabel('LBL_Zip:', $siteLangId) . $address['addr_zip'] . '<br>' : ''; ?>
                         </p>
                         <p class="phone-txt">
                             <i class="fas fa-mobile-alt"></i>
-                            <?php echo (strlen($address['addr_phone'])>0) ? Labels::getLabel('LBL_Phone:', $siteLangId).$address['addr_phone'].'<br>':''; ?>
+                            <?php echo (strlen($address['addr_phone']) > 0) ? Labels::getLabel('LBL_Phone:', $siteLangId) . $address['addr_phone'] . '<br>' : ''; ?>
                         </p>
                     </address>
                 </div>
