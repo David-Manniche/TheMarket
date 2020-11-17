c53b4d1d1fe7a4147182096ccdf57bea4b9fed79
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-11-17 18:17:42 +0530

    task-66150-Add link class to order ID on payment success page

--- application/views/custom/payment-success.php
+++ application/views/custom/payment-success.php
@@ -41,16 +41,16 @@ array_walk($orderFulFillmentTypeArr, function ($row) use (&$fulfillmentType) {
                             <h3>
                                 <?php
                                 if (Orders::ORDER_PRODUCT == $orderInfo['order_type']) {
-                                    $msg = Labels::getLabel('LBL_YOUR_ORDER_#{ORDER-ID}_HAS_BEEN_PLACED!', $siteLangId);
+                                    $msg = Labels::getLabel('LBL_YOUR_ORDER_{ORDER-ID}_HAS_BEEN_PLACED!', $siteLangId);
                                     $orderDetailUrl = UrlHelper::generateUrl('Buyer', 'viewOrder', array($orderInfo['order_id']));
-                                    $orderDetailLinkHtml = '<a href="' . $orderDetailUrl . '">'. $orderInfo['order_id'] . '</a>';
+                                    $orderDetailLinkHtml = '<a href="' . $orderDetailUrl . '" class="link">#' . $orderInfo['order_id'] . '</a>';
                                 } else {
                                     $msg = Labels::getLabel('LBL_ORDER_#{ORDER-ID}_TRANSACTION_COMPLETED!', $siteLangId);
                                     $orderProducts = current($orderInfo['orderProducts']);
                                     $orderDetailUrl = UrlHelper::generateUrl('Seller', 'viewSubscriptionOrder', array($orderProducts['ossubs_id']));
                                     $orderDetailLinkHtml = $orderInfo['order_id'];
-                                    if ( isset($orderProducts['ossubs_id']) ) {
-                                        $orderDetailLinkHtml = '<a href="' . $orderDetailUrl . '">'. $orderInfo['order_id'] . '</a>';
+                                    if (isset($orderProducts['ossubs_id'])) {
+                                        $orderDetailLinkHtml = '<a href="' . $orderDetailUrl . '">' . $orderInfo['order_id'] . '</a>';
                                     }
                                 }
                                 $msg = CommonHelper::replaceStringData($msg, ['{ORDER-ID}' => $orderDetailLinkHtml]);
