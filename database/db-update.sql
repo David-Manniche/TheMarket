b0a89dbcbddb4502f53e2048f5b6a7c7b7792bed
Anup Rawat <anup.rawat@ablysoft.com>
2020-12-02 10:49:12 +0530

    release test version TV-9.2.3.20201201

--- database/db-update.sql
+++ database/db-update.sql
@@ -1229,3 +1229,4 @@ ALTER TABLE `tbl_countries` CHANGE `country_id` `country_id` INT(11) NOT NULL AU
 UPDATE `tbl_plugins` SET `plugin_identifier` = '2Checkout' WHERE `plugin_code` = 'Twocheckout';
 ALTER TABLE `tbl_coupons_hold_pending_order` CHANGE `ochold_order_id` `ochold_order_id` VARCHAR(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `tbl_notifications` CHANGE `notification_user_id` `notification_user_id` INT(11) NOT NULL;
+-- ---------------------TV-9.2.3.20201201------------------
