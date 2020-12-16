81e036c3cc8be74dd0bffe2947095182c6e6ce6c
Satbir Kaushik <satbir.kaushik@fatbit.in>
2020-12-16 11:08:33 +0530

    task-70159-Home api response updated

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -74,7 +74,6 @@ class HomeController extends MyAppController
         }
 
         if (true === MOBILE_APP_API_CALL) {
-            $this->set('displayProductNotAvailableLable', $displayProductNotAvailableLable);
             $this->_template->render();
             die;
         }
